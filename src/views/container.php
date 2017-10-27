ba038b04328ab2a8756b37b6b274696ab7384c1e
Rose Cox <rose@purpleprodigy.com>
2017-10-27 10:50:09 +1000

    Clean up PHP

--- src/views/container.php
+++ src/views/container.php
@@ -2,9 +2,9 @@
 
 use PurpleProdigy\Gallery\Template as Template;
 
-$is_use_term_container_true =  ( isset ( $use_term_container ) && $use_term_container ) ;
+$is_use_term_container_true = ( isset ( $use_term_container ) && $use_term_container );
 
-if ( $is_use_term_container_true )  ?>
+if ( $is_use_term_container_true ) ?>
 <div class="gallery-container">
     <h1>Gallery</h1>
 	<?php
